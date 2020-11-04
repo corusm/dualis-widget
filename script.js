7c65ce0b0ae777a2b8a8bb5a03acc7bb2a36e9a3
Julian Yaman <julianyaman@posteo.eu>
2020-11-04 17:32:28 +0100

    Adding placeholder design for small widget

--- script.js
+++ script.js
@@ -10,3 +10,48 @@
 
 
 // Widget
+// Widget Parameter => E-Mail & Password
+let MEDIUMWIDGET = (config.widgetFamily === 'medium') ? true : false
+
+const widget = await createWidget()
+widget.setPadding(0,0,0,0)
+
+if (!config.runsInWidget){
+    if (MEDIUMWIDGET){
+        await widget.presentMedium()
+    } else {
+        await widget.presentSmall()
+    }
+}
+
+Script.setWidget(widget)
+Script.complete()
+
+async function createWidget(){
+    const list = new ListWidget()
+
+    const headerRow = list.addStack();
+    const headerText = headerRow.addText("âš”ï¸ Dualis")
+    headerText.textColor = new Color('e74c3c', 1)
+    const headerTextExtended = headerRow.addText(" - Noten")
+    headerText.font = Font.mediumSystemFont(16)
+    headerTextExtended.font = Font.mediumSystemFont(16)
+    headerRow.setPadding(0,0,0,0)
+    headerRow.centerAlignContent()
+    headerRow.addSpacer(3)
+    list.addSpacer(3)
+
+    const gradeRow = list.addStack()
+    gradeRow.layoutVertically()
+    gradeRow.centerAlignContent()
+
+    let padding = (MEDIUMWIDGET) ? 5 : 10
+    gradeRow.addText("Fach 1: 2,9")
+    gradeRow.addText("Fach 2: 2,4")
+    gradeRow.addText("Fach 3: WKL")
+    gradeRow.backgroundColor = new Color('cccccc', 0.1)
+    gradeRow.cornerRadius = 8
+    gradeRow.setPadding(8,8,8,8)
+
+    return list
+}
